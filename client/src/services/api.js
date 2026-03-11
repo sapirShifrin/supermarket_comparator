a96ecb41ab5fa61a86b9eae40f61d8462c495363
sapirShifrin <sapirmaz123@gmail.com>
2026-03-11 08:44:27 +0200

    Demo live

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,8 +2,10 @@
 
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // כתובת השרת שלך
+  baseURL: API_URL,
 });
 
 api.interceptors.request.use((config) => {
