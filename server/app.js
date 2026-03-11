7aef911963e465380ab03d9dd134b8dc42f2cac6
sapirShifrin <sapirmaz123@gmail.com>
2026-03-11 09:25:34 +0200

    start render

--- server/app.js
+++ server/app.js
@@ -9,7 +9,12 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 // הגשה סטטית של התמונות: http://localhost:5000/uploads/<filename>
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
